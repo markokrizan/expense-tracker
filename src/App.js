9040d5814d6ab490990e0e365293b0732aa72ac0
Marko Krizan <markokrizan64@gmail.com>
2020-12-09 19:01:54 +0100

    Use sass instead of css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
